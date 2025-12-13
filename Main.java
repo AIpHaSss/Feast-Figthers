3edd01baac13c5fc3cf40255e00779d4b5a6c8cc
AIpHaSss <raprap.real.1@gmail.com>
2025-12-13 18:07:50 +0800

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-        // Use Main as the single entry point that launches the GameLauncher on the EDT
-        SwingUtilities.invokeLater(() -> new GameLauncher());
-    }
-}
